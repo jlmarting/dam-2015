1632bab95d307f3e658c55c238275454e7855c7d
JL <jlmartingalvan@gmail.com>
2015-02-26 19:25:28 +0100

    Ejercicio Javascript resuelto. Pendiente ampliar a blur y refactorizar

---
+++ JavaScript/final/assets/js/main.js
@@ -0,0 +1,8 @@
+document.onload = (function(){
+
+    var form = document.getElementsByTagName('form')[0];
+    form.validate();
+
+
+
+})();
--- JavaScript/final/assets/js/validator.js
+++ JavaScript/final/assets/js/validator.js
@@ -1,23 +1,116 @@
-
-
 // Validación para todos los formularios
 HTMLFormElement.prototype.validate = function() {
-    var validador = {
-        required : function(val){
-            return false;
+
+    // Utilidades
+    var validator = {
+        required: function(val) {
+            console.log('required: ' + val.value);
+            return val !== undefined &&
+                val !== null &&
+                !/^\s+$/.test(val) &&
+                val.length>0;
+
         },
-        email : function(val) {
-            return false;
+        email: function(val) {
+            console.log('email: ' + val);
+            if (/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/.test(val)) {
+                return true;
+            } else
+                return false;
+        },
+        checkbox: function(val) {
+            console.log('checkbox: ' + val);
+            if (val.checked == true) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        password: function(val) {
+            return this.required(value) &&
+                value.length > 6 &&
+                /[a-z]/.test(val) &&
+                /[A-Z]/.test(val) &&
+                /[0-9]/.test(val);
+ 
+        },
+        maxlong: function(val) {
+            if (val.length < 50) {
+                return true;
+            } else {
+                return false;
+            }
+
         }
     };
 
-    var  required = this.querySelectorAll('.required');
 
-    var validate = function(){
+    /*
+    Tenemos que validar cada campo. El tipo de validación a realizar estará especificada en la clase.
+    Recogeremos todos los campos que sean requeridos.
+     */
+    var required = this.querySelectorAll('.required');
+    var email = this.querySelectorAll('.email');
+    var password = this.querySelectorAll('.password');
+    var textarea = this.querySelectorAll('.maxlong');
 
-    };
+    var validate = function(e) {
+        console.log('Validando formulario...');
+
+        var err = [];
+
+        //Comprobación de required
+        for (var i = 0; i < required.length; i++) {
+
+            if (required[i].type == "checkbox") {
+                if (validator.checkbox(required[i])) {
+                    console.log('Ok');
+                } else {
+                    console.log('Meeec!');
+                    err.push(this.name + ' no está marcado');
+                }
+
+
+            } else {
+                if (validator.required(required[i])) {
+                    console.log('Ok');
+                } else {
+                    console.log('Meeec!');
+                    err.push(this.name + ' es obligatorio');
+                }
+
+            }
+
+        }
+
+        //Comprobación de mail
+        for (var i = 0; i < email.length; i++) {
+            if (validator.email(email[i].value)) {
+                console.log('ok');
+            } else {
+                console.log('meec!');
+                err.push('email');
+            }
+        }
+
+        //Comprobación de textarea
+        for (var i = 0; i < textarea.length; i++) {
+            if (validator.maxlong(textarea[i].value)) {
+                console.log('ok');
+            } else {
+                console.log('meec!');
+                err.push(this.name + ' texto demasiado largo');
+        }
+    }
+
+    //if (err.length > 0) {
+        e.preventDefault();
+        alert(err.join('\n'));
+    //  }
+
 };
 
-
-// Listener en submit
+// Listener en formulario para submit
 this.addEventListener('submit', validate, false);
+
+};
